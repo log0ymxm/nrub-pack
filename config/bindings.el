af386f6e1d2af1a24fe5cfc1ad0b827ba5d18e78
Paul English <paul@onfrst.com>
2013-10-22 14:44:32 -0600

    Use ack for most searching but keep find-grep-dired around, make sure eval-and-replace is defined

--- config/bindings.el
+++ config/bindings.el
@@ -4,4 +4,15 @@
 ;;(define-key global-map (kbd "C-+") 'text-scale-increase)
 ;;(define-key global-map (kbd "C--") 'text-scale-decrease)
 
-(global-set-key (kbd "C-c s") 'find-grep-dired)
+(global-set-key (kbd "C-c C-s") 'find-grep-dired)
+(global-set-key (kbd "C-c s") 'ack)
+
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
